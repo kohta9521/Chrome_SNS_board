ef097400f69b7fb5a2840e93b4cd74d468c7f6a8
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-28 18:45:20 +0900

    add all firls upload

--- chrome-extensions/alert.js
+++ chrome-extensions/alert.js
@@ -2,6 +2,10 @@ function showAlert() {
   alert("今日も楽しく開発しよう！");
 }
 
+document.getElementById("alertButton").onClick = function () {
+  showAlert();
+};
+
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
